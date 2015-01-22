0ff6c391b0a55d205a928c99b8af69bdf05f21a4
frc2606 <frc2606@frc2606-2015a>
2015-01-22 11:40:51 -0600

    commiting

---
+++ src/org/usfirst/frc/team2606/robot/commands/TestCommandGroupLeft.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2606.robot.commands;
+
+import org.usfirst.frc.team2606.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class TestCommandGroupLeft extends CommandGroup {
+    
+    public  TestCommandGroupLeft() {
+        addSequential(new CloseClaw());
+        addSequential(new SetSwivelSetpoint(248));
+    }
+}
---
+++ src/org/usfirst/frc/team2606/robot/commands/TestCommandGroupRight.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team2606.robot.commands;
+
+import org.usfirst.frc.team2606.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class TestCommandGroupRight extends CommandGroup {
+    
+    public  TestCommandGroupRight() {
+    	addSequential(new OpenClaw());
+        addSequential(new SetSwivelSetpoint(0));
+    }
+}
