c7163e7c53fcebca0df78d42bb84b5f015c7efe0
frc2606 <frc2606@frc2606-2015a>
2015-01-25 10:39:01 -0600

    add DriveStraight.java, made DriveTrain compatible

---
+++ src/org/usfirst/frc/team2606/robot/commands/DriveStraight.java
@@ -0,0 +1,70 @@
+package org.usfirst.frc.team2606.robot.commands;
+
+import org.usfirst.frc.team2606.robot.Robot;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStraight extends Command {
+	private PIDController pid;
+	private static final double driveKP = 4.0, gyroKP = 1 / 45.0;
+
+	public DriveStraight(double distance) {
+		requires(Robot.drivetrain);
+		Robot.drivetrain.setGyroDesiredHeading();
+		pid = new PIDController(driveKP, 0, 0, new PIDSource() {
+			public double pidGet() {
+				return Robot.drivetrain.getDistanceInFeet();
+			}
+		}, new PIDOutput() {
+			public void pidWrite(double d) {
+				double gyroError = gyroKP * Robot.drivetrain.GyroAngleError();
+				if (gyroError > 0) {
+					Robot.drivetrain.drive(d-gyroError, d);
+				} else if (gyroError < 0) {
+					Robot.drivetrain.drive(d, d+gyroError);
+				} else {
+					Robot.drivetrain.drive(d, d);
+				}
+			}
+		});
+		pid.setAbsoluteTolerance(0.01);
+		pid.setSetpoint(distance);
+	}
+
+	protected void initialize() {
+		// Get everything in a safe starting state.
+		Robot.drivetrain.reset();
+		// Called just before this Command runs the first time
+		pid.reset();
+		pid.enable();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return pid.onTarget();
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		// Stop PID and the wheels
+		pid.disable();
+		Robot.drivetrain.drive(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+
+}
--- src/org/usfirst/frc/team2606/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2606/robot/subsystems/DriveTrain.java
@@ -23,8 +23,9 @@ public class DriveTrain extends Subsystem {
 	private SpeedController leftMotor = RobotMap.LEFT_MOTOR;
 	private SpeedController rightMotor = RobotMap.RIGHT_MOTOR;
 	private RobotDrive drive;
-
-	// private Encoder leftEncoder , rightEncoder;
+	private Gyro gyro = RobotMap.GYRO;
+	private double gyroDesiredHeading;
+	private Encoder leftEncoder, rightEncoder;
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
@@ -32,36 +33,10 @@ public class DriveTrain extends Subsystem {
 	public DriveTrain() {
 		super();
 		drive = new RobotDrive(leftMotor, rightMotor);
-		// leftEncoder = new Encoder(1, 2);
-		// rightEncoder = new Encoder(3, 4);
-
-		// Encoders may measure differently in the real world and in
-		// simulation. In this example the robot moves 0.042 barleycorns
-		// per tick in the real world, but the simulated encoders
-		// simulate 360 tick encoders. This if statement allows for the
-		// real robot to handle this difference in devices.
-		// if (Robot.isReal()) {
-		// left_encoder.setDistancePerPulse(0.042);
-		// right_encoder.setDistancePerPulse(0.042);
-		// } else {
-		// // Circumference in ft = 4in/12(in/ft)*PI
-		// left_encoder.setDistancePerPulse((4.0 / 12.0 * Math.PI) / 360.0);
-		// right_encoder.setDistancePerPulse((4.0 / 12.0 * Math.PI) / 360.0);
-		// }
-
-		// Let's show everything on the LiveWindow
-		// LiveWindow.addActuator("Drive Train", "Front_Left Motor",
-		// (Talon) front_left_motor);
-		// LiveWindow.addActuator("Drive Train", "Back Left Motor",
-		// (Talon) back_left_motor);
-		// LiveWindow.addActuator("Drive Train", "Front Right Motor",
-		// (Talon) front_right_motor);
-		// LiveWindow.addActuator("Drive Train", "Back Right Motor",
-		// (Talon) back_right_motor);
-		// LiveWindow.addSensor("Drive Train", "Left Encoder", left_encoder);
-		// LiveWindow.addSensor("Drive Train", "Right Encoder", right_encoder);
-		// LiveWindow.addSensor("Drive Train", "Rangefinder", rangefinder);
-		// LiveWindow.addSensor("Drive Train", "Gyro", gyro);
+		leftEncoder = new Encoder(1, 2);
+		rightEncoder = new Encoder(3, 4);
+		leftEncoder.setDistancePerPulse((6.0 / 12.0 * Math.PI) / 160.0);
+		rightEncoder.setDistancePerPulse((6.0 / 12.0 * Math.PI) / 160.0);
 	}
 
 	/**
@@ -109,15 +84,29 @@ public class DriveTrain extends Subsystem {
 	 * Reset the robots sensors to the zero states.
 	 */
 	public void reset() {
-		// left_encoder.reset();
-		// right_encoder.reset();
+		leftEncoder.reset();
+		rightEncoder.reset();
+		gyro.reset();
 	}
 
-	/**
-	 * @return The distance driven (average of left and right encoders).
-	 */
-	// public double getDistance() {
-	// return (left_encoder.getDistance() + right_encoder.getDistance()) / 2;
-	// }
+	public void setGyroDesiredHeading() {
+		gyroDesiredHeading = gyro.getAngle();
+	}
+
+	public double getGyroRealHeading() {
+		return gyro.getAngle();
+	}
+
+	public double GyroAngleError() {
+		return gyro.getAngle()-gyroDesiredHeading;
+	}
+
+	public double getDistanceInFeet() {
+		return (leftEncoder.getDistance() + rightEncoder.getDistance()) / 2.0;
+	}
+
+	public double getDistanceInInches() {
+		return (leftEncoder.getDistance() + rightEncoder.getDistance()) * 6.0;
+	}
 
 }
