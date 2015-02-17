256c5c043db47418233739094e803d6e6d4037d9
Dan Little <danlittle@yahoo.com>
2015-02-17 06:24:12 -0600

    Moved camera drawing to a separate thread to prevent jerkyness

--- src/org/usfirst/frc/team2606/robot/Robot.java
+++ src/org/usfirst/frc/team2606/robot/Robot.java
@@ -22,6 +22,8 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import java.lang.Thread;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -38,8 +40,56 @@ public class Robot extends IterativeRobot {
 	public static Camera camera;
 	
 	// used for drawing on the camera image.
-    int session;
-    Image frame;
+	int session;
+	Image frame;
+
+	/** Updates camera in the background 
+	 *
+	 */
+	private class CameraDrawing extends Thread {
+		private boolean mRun = true;
+
+		CameraDrawing() {
+			frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+			// the camera name (ex "cam0") can be found through the roborio web interface
+			session = NIVision.IMAQdxOpenCamera("cam0",
+				NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+			NIVision.IMAQdxConfigureGrab(session);
+			NIVision.IMAQdxStartAcquisition(session);
+		}
+
+		public void run() {
+			/**
+			* grab an image, draw the circle, and provide it for the camera server
+			* which will in turn send it to the dashboard.
+			*/
+			NIVision.Rect rect = new NIVision.Rect(150, 150, 100, 460);
+			NIVision.Rect rect2 = new NIVision.Rect(120, 150, 140, 300);
+
+			while(mRun) {
+				NIVision.IMAQdxGrab(session, frame, 1);
+				NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+				    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
+
+				CameraServer.getInstance().setImage(frame);
+				NIVision.imaqDrawShapeOnImage(frame, frame, rect2,
+				    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
+				CameraServer.getInstance().setImage(frame);
+				/** robot code here! **/
+				Timer.delay(0.005);		// wait for a motor update time
+			}
+			// stop acquiring an image once the loop is broken
+			NIVision.IMAQdxStopAcquisition(session);
+		}
+
+		public void stopDrawing() {
+			mRun = false;
+		}
+	}
+
+	CameraThread cameraDrawer;
+
+
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -53,12 +103,6 @@ public class Robot extends IterativeRobot {
 		camera = new Camera();
 		
 
-        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-
-        // the camera name (ex "cam0") can be found through the roborio web interface
-        session = NIVision.IMAQdxOpenCamera("cam0",
-                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        NIVision.IMAQdxConfigureGrab(session);
 	}
 
 	public void disabledPeriodic() {
@@ -79,11 +123,10 @@ public class Robot extends IterativeRobot {
 
 	public void teleopInit() {
 		reset();
-		// if (autonomousCommand != null) autonomousCommand.cancel();
-		
-		// vision code.
-        NIVision.IMAQdxStartAcquisition(session);
 
+		// start the camera thread.
+		cameraDrawer = new CameraThread();
+		cameraDrawer.run();
 	}
 
 	/**
@@ -91,8 +134,7 @@ public class Robot extends IterativeRobot {
 	 * to reset subsystems before shutting down.
 	 */
 	public void disabledInit() {
-        NIVision.IMAQdxStopAcquisition(session);
-
+		cameraDrawer.stopDrawing();	
 	}
 
 	/**
@@ -103,54 +145,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard
 				.putNumber("Encoder Value", RobotMap.SWIVEL_ENCODER.get());
 		log();
-
-        /**
-         * grab an image, draw the circle, and provide it for the camera server
-         * which will in turn send it to the dashboard.
-         */
-        NIVision.Rect rect = new NIVision.Rect(150, 150, 100, 460);
-        NIVision.Rect rect2 = new NIVision.Rect(120, 150, 140, 300);
-        //while (isOperatorControl() && isEnabled()) {
-
-            NIVision.IMAQdxGrab(session, frame, 1);
-            NIVision.imaqDrawShapeOnImage(frame, frame, rect,
-                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
-            
-            CameraServer.getInstance().setImage(frame);
-            NIVision.imaqDrawShapeOnImage(frame, frame, rect2,
-                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
-            CameraServer.getInstance().setImage(frame);
-            /** robot code here! **/
-           // Timer.delay(0.005);		// wait for a motor update time
-       // }
-		
 	}
-	
-	
-    public void operatorControl() {
-        NIVision.IMAQdxStartAcquisition(session);
-
-        /**
-         * grab an image, draw the circle, and provide it for the camera server
-         * which will in turn send it to the dashboard.
-         */
-        NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
-        NIVision.Rect rect2 = new NIVision.Rect(10, 10, 100, 100);
-        while (isOperatorControl() && isEnabled()) {
-
-            NIVision.IMAQdxGrab(session, frame, 1);
-            NIVision.imaqDrawShapeOnImage(frame, frame, rect,
-                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-            CameraServer.getInstance().setImage(frame);
-            NIVision.imaqDrawShapeOnImage(frame, frame, rect2,
-                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-            /** robot code here! **/
-            Timer.delay(0.005);		// wait for a motor update time
-        }
-        NIVision.IMAQdxStopAcquisition(session);
-        //Log.e("2606", "Got to operator Control");
-    }
-
 	
 	public void log(){
 		drivetrain.log();
